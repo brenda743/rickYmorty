89d50e0790ace4e30e2711b7275c332f0b082ba1
brenda743 <brendiaz0202@gmail.com>
2024-11-25 03:04:44 -0300

    Add files via upload

---
+++ ip-public-repo-2c2024/app/layers/services/services.py
@@ -0,0 +1,56 @@
+# capa de servicio/lógica de negocio
+
+from..transport import transport
+from..persistence import repositories
+from..utilities import translator 
+from django.contrib.auth import get_user
+
+def getAllImages(input=None):
+    json_collection =  transport.getAllImages(input) 
+
+
+    images =[]
+    for i in range(len(json_collection)):
+        image = translator.fromRequestIntoCard(json_collection[i])
+        images.append(image)
+   
+    return images  
+
+
+
+def imaginesRelacionadas(search_msg: str) -> list:
+        json_collection =  transport.getAllImages(search_msg) 
+        imagesRelacionadas = []
+        for i in range(len(json_collection)):
+            image = translator.fromRequestIntoCard(json_collection[i])
+            if search_msg.lower() in image.name.lower() or search_msg.lower() in image.status.lower():
+                imagesRelacionadas.append(image)
+
+        return imagesRelacionadas
+
+# añadir favoritos (usado desde el template 'home.html')
+def saveFavourite(request):
+    fav = '' # transformamos un request del template en una Card.
+    fav.user = '' # le asignamos el usuario correspondiente.
+
+    return repositories.saveFavourite(fav) # lo guardamos en la base.
+
+# usados desde el template 'favourites.html'
+def getAllFavourites(request):
+    if not request.user.is_authenticated:
+        return []
+    else:
+        user = get_user(request)
+
+        favourite_list = [] # buscamos desde el repositories.py TODOS los favoritos del usuario (variable 'user').
+        mapped_favourites = []
+
+        for favourite in favourite_list:
+            card = '' # transformamos cada favorito en una Card, y lo almacenamos en card.
+            mapped_favourites.append(card)
+
+        return mapped_favourites
+
+def deleteFavourite(request):
+    favId = request.POST.get('id')
+    return repositories.deleteFavourite(favId) # borramos un favorito por su ID.
---
+++ ip-public-repo-2c2024/app/views.py
@@ -0,0 +1,50 @@
+# capa de vista/presentación
+
+from django.shortcuts import redirect, render
+from .layers.services import services
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+
+
+def index_page(request):
+    return render(request, 'index.html')
+
+# esta función obtiene 2 listados que corresponden a las imágenes de la API y los favoritos del usuario, y los usa para dibujar el correspondiente template.
+# si el opcional de favoritos no está desarrollado, devuelve un listado vacío.
+def home(request):
+    images = services.getAllImages()
+    favourite_list = []
+    return render(request, 'home.html', { 'images': images, 'favourite_list': favourite_list })
+
+
+
+def search(request):
+    search_msg = request.POST.get('query', '')
+
+    if search_msg != '':
+        imagesRe=services.imaginesRelacionadas(search_msg)
+        return render(request, 'home.html', { 'images': imagesRe , 'query': search_msg})
+
+
+    else:
+        return redirect('home')
+
+
+
+# Estas funciones se usan cuando el usuario está logueado en la aplicación.
+@login_required
+def getAllFavouritesByUser(request):
+    favourite_list = []
+    return render(request, 'favourites.html', { 'favourite_list': favourite_list })
+
+@login_required
+def saveFavourite(request):
+    pass
+
+@login_required
+def deleteFavourite(request):
+    pass
+
+@login_required
+def exit(request):
+    pass
